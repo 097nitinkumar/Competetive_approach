ef1d54e4af6b192d33ec96cf71526765140df114
Nitin kumar <36824328+097nitinkumar@users.noreply.github.com>
2018-04-01 19:33:44 +0530

    Update Macros.cpp

--- Macros.cpp
+++ Macros.cpp
@@ -6,8 +6,8 @@
 */
 #define EB emplace_back
 //emplace_back is faster than push_back
-#define NL <<"\n"
-//"\n" is faster than endl as nedl has fflush included
+#define NL cout<<"\n"
+//"\n" is faster than endl as endl has fflush included
 #define GCD(x,y) _gcd(x, y)
 //Buildin function to find hcf
 #define CPYAR(Arr,x,Arr1) copy_n(Arr, x, Arr1)
@@ -33,4 +33,43 @@ n = n << 1;   // Multiply n with 2
 n = n << p;   // Multiply by 2^p
 n = n >> 1;   // Divide n by 2
 n = n >> p;   // Divide n by 2^p
+*/
+/*
+//EVEN OR ODD
+if (num & 1)
+   cout << "ODD";
+else
+   cout << "EVEN";
+*/
+#define vector <int> VI
+#define vector <long long> VLL
+#define vector <string> VS
+#define long long ll
+#define unsigned long long ull
+#define MIN(a,b) a=min((a),(b))
+#define MAX(a,b) a=max((a),(b))
+/////////////////////////////////////////////////////////////////////typedef complex<Real> P;
+#define B begin()
+#define E end()
+
+/*
+Approach 1
+//int no_of_cols = 5;
+//int no_of_rows = 10;
+//int initial_value = 0;
+#define vector<vector<int>> VVI 
+//VVI matrix;
+//matrix.resize(no_of_rows, vector<int>(no_of_cols, initial_value));
+*/
+
+/*
+Approach 2
+vector<vector<int>> matrix;
+//initialize the 1D vector you would like to insert into matrix
+vector<int> row;
+//initializing row with values
+row.push_back(val1);
+row.push_back(val2);
+//now inserting values into matrix
+matrix.push_back(row);
 */
