8f01dfa5510a61f4b0fa75790fb53c5542122d57
Nitin kumar <36824328+097nitinkumar@users.noreply.github.com>
2018-04-01 19:56:27 +0530

    Create Headers.cpp

---
+++ Headers.cpp
@@ -0,0 +1,47 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <random>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+
+
+
+#include <ctype.h>
+
+#include <queue>
+#include <cstring>
+#include <set>
+#include <list>
+#include <map>
+#include <random>
+#include <unordered_map>
+#include <stdio.h>
+
+
+
+
+
+using namespace std;
