cff003679931844903d695aa2d0c0d8fac422578
Nitin kumar <36824328+097nitinkumar@users.noreply.github.com>
2018-04-21 23:56:17 +0530

    21 April

--- Notes/Special case error/main.cpp
+++ Notes/Special case error/main.cpp
@@ -8,6 +8,15 @@ int main()
     p=0;
     d++,p--;//no error
     //double i = 0,int d = 0;//error
+    
+    /*
+    for (int i = 0, double d = 0.0; i < 10; i++)
+    {
+      d += .4;
+      cout << d << endl;
+    }//hence error
+    */
+    
     for (double i = 0, d = 0; i < 10; i++)
     {
       d +=1;
